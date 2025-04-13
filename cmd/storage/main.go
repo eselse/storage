8aaea61ddbd84d615a17ba8c82614a4b2c691bd8
eselse <ilikewebdevelopment@gmail.com>
2025-04-13 12:26:07 +0400

    After test run

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/eselse/storage/internal/storage"
 )
 
 func main() {
 	st := storage.NewStorage()
-	fmt.Println("It works.", st)
+
+	file, err := st.Upload("test.txt", []byte("Hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("It works.", file)
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,7 +1,18 @@
 package storage
 
+import "github.com/eselse/storage/internal/file"
+
 type Storage struct{}
 
 func NewStorage() *Storage {
 	return &Storage{}
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
+	return file.NewFile(filename, blob)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// return newFile
 }
